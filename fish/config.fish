fb55a212897b88ea0ad164a51d836ccee60204f6
McShinyShoe <mcshinyshoe@protonmail.com>
2026-04-29 23:15:48 +0700

    feat: Added direnv and sccache integration to fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,8 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
+# fish (~/.config/fish/config.fish)
+direnv hook fish | source
+
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
